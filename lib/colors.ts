2b2dcfd0460962479254b0d6df28ad9fd78cbd6d
skswanke <skswanke@gmail.com>
2023-03-10 11:45:45 -0500

    update hover colors

--- lib/colors.ts
+++ lib/colors.ts
@@ -1,5 +1,5 @@
-export const HOVER_BG = "#f8f8f8";
-export const HOVER_BG_DARK = "#282828";
+export const HOVER_BG = "rgba(0, 0, 0, 0.05)";
+export const HOVER_BG_DARK = "rgba(255, 255, 255, 0.16)";
 export const SECONDARY_TEXT = "#585858";
 export const PRIMARY_TEXT = "#000000";
 export const SECONDARY_TEXT_DARK = "#a8a8a8";
