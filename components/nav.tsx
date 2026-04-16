8981c460846b4309e1106fae1609ab054df87694
Sam Swanke <skswanke@users.noreply.github.com>
2026-04-16 13:36:23 -0400

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -73,7 +73,7 @@ export default function Nav({ handleClose }: Props) {
         >
           Resume
         </NavItem>
-        <A onClick={handleClose} href="https://github.com/skswanke" aria-label="Github Profile" target="#">
+        <A onClick={handleClose} href="https://github.com/skswanke" ariaLabel="Github Profile" target="#">
           <Github />
         </A>
       </NavContainer>
